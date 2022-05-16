935df5b8c7b1fb0d57713b9434a5cf2b3c2a29fc
hdoonah <Huda.alaslami@gmail.com>
2022-05-16 18:44:30 +0300

    Initial commit

---
+++ huda_code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  huda_code
+//
+//  Created by HUDA ALSHAMMARI on 15/10/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ huda_code/huda_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  huda_codeApp.swift
+//  huda_code
+//
+//  Created by HUDA ALSHAMMARI on 15/10/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct huda_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
